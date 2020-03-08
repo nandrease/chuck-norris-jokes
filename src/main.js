7cea185ecf7c5f1aa3bb295add9ee88e5d7b4e96
nandrease <neeme.andreas.eller@gmail.com>
2020-03-08 11:17:27 +0200

    Add BulmaCSS

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from "vue";
-import VueCompositionApi from '@vue/composition-api';
+import VueCompositionApi from "@vue/composition-api";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "./../node_modules/bulma/css/bulma.css";
 
 Vue.config.productionTip = false;
 Vue.use(VueCompositionApi);
